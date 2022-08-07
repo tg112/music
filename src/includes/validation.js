183eb5ec7650e93723e8b7a969898774f377dc1d
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-08-07 14:52:14 +0900

    Add and apply validation rules

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,11 +1,25 @@
-import { required } from "@vee-validate/rules";
-import { Field as VeeField, Form as VeeForm, defineRule } from "vee-validate";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+} from "@vee-validate/rules";
+import {
+  Field as VeeField,
+  Form as VeeForm,
+  defineRule,
+  ErrorMessage,
+} from "vee-validate";
 
 export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
 
     defineRule("required", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaSpaces);
   },
 };
