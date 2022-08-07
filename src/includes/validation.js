6a3387549fdaddd5e3432cc8e517e91d0a8b43b8
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-08-07 22:26:09 +0900

    Set validate triggers

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -55,6 +55,10 @@ export default {
           : `The field ${ctx.field} is invalid`;
         return message;
       },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
     });
   },
 };
