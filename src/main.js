10f71c8cd1d4386c09882cef3ccb10eabbe1ff76
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-08-08 23:52:20 +0900

    add: Install firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import VeeValidatePlugin from "./includes/validation";
+import "./includes/firebase";
 
 import App from "./App.vue";
 import router from "./router";
