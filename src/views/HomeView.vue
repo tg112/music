90e0db90d5604e0d40a10389c52e8a53d7baa2ff
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-08-21 22:24:38 +0900

    memo: put 2ways to render directives

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,6 +28,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div
         class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+        v-icon.yellow.right="'headphones-alt'"
         v-icon-secondary="{ icon: 'headphones-alt', right: true }"
       >
         <span class="card-title">Songs</span>
